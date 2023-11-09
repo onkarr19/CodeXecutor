eda41c94171b0ad75ef5f573f861c529793c35d2
onkar <onkar.chendage@gmail.com>
2023-11-09 23:12:30 +0530

    add model job

---
+++ models/job.go
@@ -0,0 +1,17 @@
+package models
+
+import "time"
+
+type Job struct {
+	ID       string    `json:"id"`       // unique identifier
+	Language string    `json:"language"` // Programming language used in the code
+	Code     string    `json:"code"`     // The user's code
+	Time     time.Time `json:"time"`     // The time of submission
+}
+
+// DockerConfig represents the configuration for the Docker container.
+type DockerConfig struct {
+	Image    string `json:"image"`    // Docker image name, e.g., "python:3.9"
+	Code     string `json:"code"`     // User's code to be executed
+	Language string `json:"language"` // Programming language (used for selecting the image)
+}
