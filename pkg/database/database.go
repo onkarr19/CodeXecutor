4df330ff3b9742b022334284c35e36ddc88e4050
onkar <onkar.chendage@gmail.com>
2023-11-14 12:23:10 +0530

    security, db - future scope

---
+++ pkg/database/database.go
@@ -0,0 +1,14 @@
+package database
+
+// import (
+// 	"database/sql"
+// 	// _ "github.com/driver/sql-driver/mysql"
+// )
+
+// func ConnectDB() (*sql.DB, error) {
+// 	// Establish a connection to your database
+// }
+
+// func StoreCodeResults(db *sql.DB, code, results string) error {
+// 	// Store code and results in the database
+// }
