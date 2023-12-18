4182178932de2d457009461408e170b3b8023d42
onkar <onkar.chendage@gmail.com>
2023-12-19 00:34:56 +0530

    fix: add redis as well

--- scripts/pull_images.sh
+++ scripts/pull_images.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define the list of Docker images to pull
-images=("gcc:10.3" "python:3.9" "openjdk:11.0.12" "node:14.17" "golang:1.21")
+images=("gcc:10.3" "python:3.9" "openjdk:11.0.12" "node:14.17" "golang:1.21" "redis:latest")
 
 # Loop through the list and pull each image
 for image in "${images[@]}"; do
