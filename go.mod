045046dc451a8e01da781dad4bdd48ac9c61d45f
onkar <onkar.chendage@gmail.com>
2023-12-30 11:26:53 +0530

    add: unit tests for redis pkg

--- go.mod
+++ go.mod
@@ -2,10 +2,16 @@ module CodeXecutor
 
 go 1.21.3
 
-require github.com/gorilla/mux v1.8.1
+require (
+	github.com/gorilla/mux v1.8.1
+	github.com/stretchr/testify v1.8.4
+)
 
 require (
 	github.com/Microsoft/go-winio v0.6.1 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/distribution/reference v0.5.0 // indirect
 	github.com/docker/distribution v2.8.3+incompatible // indirect
 	github.com/docker/go-connections v0.4.0 // indirect
@@ -17,19 +23,18 @@ require (
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/opencontainers/image-spec v1.0.2 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/stretchr/testify v1.8.4 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/mod v0.13.0 // indirect
 	golang.org/x/net v0.17.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
 	golang.org/x/time v0.0.0-20191024005414-555d28b269f0 // indirect
 	golang.org/x/tools v0.14.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 	gotest.tools/v3 v3.5.1 // indirect
 )
 
 require (
 	github.com/BurntSushi/toml v1.3.2
-	github.com/cespare/xxhash/v2 v2.2.0 // indirect
-	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/docker/docker v24.0.7+incompatible
 	github.com/google/uuid v1.4.0
 	github.com/redis/go-redis/v9 v9.3.0
---
+++ pkg/redis/redis_test.go
@@ -0,0 +1,121 @@
+package redis
+
+import (
+	"CodeXecutor/models"
+	"context"
+	"encoding/json"
+	"log"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const testConfigFilePath = "test-redis-config.toml"
+
+func TestLoadRedisConfig(t *testing.T) {
+	// Create a sample Redis configuration file for testing
+	testConfigContent := `
+		[redis]
+		Addr = "localhost:6379"
+		Password = ""
+		DB = 0
+		PoolSize = 10
+		MinIdleConns = 5
+	`
+
+	err := os.WriteFile(testConfigFilePath, []byte(testConfigContent), 0644)
+	assert.NoError(t, err, "Error creating test Redis config file")
+	defer os.Remove(testConfigFilePath)
+
+	config, err := LoadRedisConfig(testConfigFilePath)
+	assert.NoError(t, err, "Error loading Redis config")
+	assert.NotNil(t, config, "Config should not be nil")
+	assert.Equal(t, "localhost:6379", config.Redis.Addr, "Incorrect Redis address in config")
+
+}
+
+func TestConnectRedis(t *testing.T) {
+	// Ensure that ConnectRedis initializes the Redis client and connects to Redis
+	client := ConnectRedis()
+	assert.NotNil(t, client, "Redis client should not be nil")
+
+	// Check if the client can perform a basic operation (Ping)
+	pong, err := client.Ping(context.Background()).Result()
+	assert.NoError(t, err, "Failed to ping Redis")
+	assert.Equal(t, "PONG", pong, "Unexpected ping response")
+}
+
+func TestEnqueueDequeueItem(t *testing.T) {
+	client := ConnectRedis()
+
+	// Create a sample job for testing
+	job := models.Job{
+		ID:       "123",
+		Language: "go",
+		Code:     "fmt.Println('Hello, World!')",
+		Time:     int(time.Now().Unix()),
+	}
+
+	// Enqueue the job
+	err := EnqueueItem(client, "test-queue", job)
+	assert.NoError(t, err, "Error enqueuing item")
+
+	// Dequeue the job
+	dequeuedJob, err := DequeueItem(client, "test-queue")
+	assert.NoError(t, err, "Error dequeuing item")
+	assert.Equal(t, job, dequeuedJob, "Enqueued and dequeued jobs should be equal")
+
+}
+
+func TestSetGetCache(t *testing.T) {
+	client := ConnectRedis()
+
+	// Set data in cache
+	key := "unique-key-2"
+	data := map[string]interface{}{"name": "John", "age": 30.0}
+	err := SetCache(client, key, data, time.Minute)
+	assert.NoError(t, err, "Error setting data in cache")
+	log.Println("working")
+
+	// Get data from cache
+	cachedData, err := GetCache(client, key)
+	assert.NoError(t, err, "Error getting data from cache")
+	assert.NotEmpty(t, cachedData, "Cached data should not be empty")
+
+	// Unmarshal the cached data and compare
+	var result map[string]interface{}
+	err = json.Unmarshal([]byte(cachedData), &result)
+	assert.NoError(t, err, "Error unmarshalling cached data")
+	assert.Equal(t, data, result, "Cached data does not match expected data")
+}
+
+func TestSetCacheExpiredGetCache(t *testing.T) {
+	client := ConnectRedis()
+	defer client.Close()
+
+	key := "expiredKey"
+	data := "testData"
+	expiration := time.Millisecond * 100 // Very short expiration time for testing
+
+	// Test SetCache with short expiration time
+	err := SetCache(client, key, data, expiration)
+	assert.NoError(t, err, "SetCache should not return an error")
+
+	// Wait for expiration
+	time.Sleep(expiration + time.Millisecond*50)
+
+	// Test GetCache for an expired key
+	_, err = GetCache(client, key)
+	assert.Error(t, err, "GetCache should return an error for an expired key")
+}
+
+func TestGetCacheKeyNotFound(t *testing.T) {
+	client := ConnectRedis()
+	defer client.Close()
+
+	// Test GetCache for a non-existent key
+	_, err := GetCache(client, "nonExistentKey")
+	assert.Error(t, err, "GetCache should return an error for a non-existent key")
+}
